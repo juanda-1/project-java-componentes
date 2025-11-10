e5cb4e4f7537452f4fc55c61b1e5e587f3b5af8f
juan <noriegajuan476@gmail.com>
2025-11-09 19:27:52 -0500

    feat: implementación de clase Vehiculo con atributos y métodos

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Creacion de objeto Vehiculo
+        Vehiculo miVehiculo = new Vehiculo(1, "Toyota", "Corolla", "Rojo", 2020);
+        miVehiculo.mostrarVehiculo();
+
+       
+
+    }
+    
+}
