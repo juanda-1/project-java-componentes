ed579a5e2a7d828d6ce500195bf13d300ce71f26
juan <noriegajuan476@gmail.com>
2025-11-09 19:33:52 -0500

    feat: implementación de clase Motor y Transmision

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,13 @@ public class Main {
         Vehiculo miVehiculo = new Vehiculo(1, "Toyota", "Corolla", "Rojo", 2020);
         miVehiculo.mostrarVehiculo();
 
-       
+         Motor motor1 = new Motor(2.0, 150, "Gasolina", 4);
+        motor1.mostrarMotor();
 
+        Transmision transmision1 = new Transmision("Automatica", 6, "Delantera", true);
+        transmision1.mostrarTransmision();
+
+    
     }
     
 }
---
+++ src/Transmision.java
@@ -0,0 +1,24 @@
+public class Transmision {
+    public String tipo;
+    public int numeroVelocidades;
+    public String traccion;
+    public boolean estado;
+
+    // Constructor
+    public Transmision(String tipo, int numeroVelocidades, String traccion, boolean estado) {
+        this.tipo = tipo;
+        this.numeroVelocidades = numeroVelocidades;
+        this.traccion = traccion;
+        this.estado = estado;
+    }
+
+    // Metodo
+
+    public void mostrarTransmision() {
+        System.out.println("==Informacion de la Transmision== ");
+        System.out.println("Tipo: " + tipo);
+        System.out.println("Numero de Velocidades: " + numeroVelocidades);
+        System.out.println("Traccion: " + traccion);
+        System.out.println("Estado: " + (estado ? "Activa" : "Inactiva"));
+    }
+}
